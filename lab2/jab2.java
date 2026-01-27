4cfa7a8e769b3d04d6ac0c4d86b2a6fe7d502fc5
nurayim <nurayim516@gmail.com>
2026-01-27 12:45:59 +0500

    lab2

--- lab2/jab2.java
+++ lab2/jab2.java
@@ -18,3 +18,52 @@ class fclass2 extends jab2 {
     }
 }
 
+//2-есеп
+//public class jab2 {
+//    void method1(){
+//        System.out.println("fclass1 класында main() әдісі");
+//    }
+//    public static void main(String[] args) {
+//        new fclass3().method1();
+//    }
+//}
+//class fclass1 extends jab2 {
+//    void method3(){
+//        System.out.println("fclass1 класының айнымалыларын енгізу үшін fclass2 әдісін құру");
+//    }
+//}
+
+//3-есеп
+//public class jab2 {
+//    void student(){
+//        System.out.println("Student класында main() әдісі");
+//    }
+//    public static void main(String[] args) {
+//        new fclass3().method1();
+//    }
+//}
+//class uniber extends jab2 {
+//    void method3(){
+//        System.out.println("uniber класы student класына");
+//    }
+//}
+//class kafedra extends jab2 {
+//    void method3(){
+//        System.out.println("uniber класы kafedra класынан мұрагерлік құру");
+//    }
+//}
+
+//4-есеп
+//public class jab2 {
+//    void Lab(){
+//        System.out.println("Lab класында main() әдісі");
+//    }
+//    public static void main(String[] args) {
+//        new fclass3().method1();
+//    }
+//}
+//class Lec extends jab2 {
+//    void method3(){
+//        System.out.println("айнымалылар енгізу үшін Lec әдісі құру");
+//    }
+//}
